56d81805e18e38169a6bd5a63a821aff573896f0
divyangana <divyanganakothari1996@gmail.com>
2024-05-21 14:04:03 +0200

    final data processing and using tsptw2 model for solution

--- main.py
+++ main.py
@@ -2,38 +2,41 @@ import math
 import random
 
 from pyscipopt import Model, quicksum
-from read_excel import expected_dict,n, e,l
+#from read_excel import read_excel_data, process_data
+from read_excel import expected_dict, e, l, n
 
-
-def mtz2tw(n, c, e, l):
-    model = Model("tsptw - mtz-strong")
+def tsptw2(n, c, e, l):
+    """tsptw2: model for the traveling salesman problem with time windows
+    (based on Miller-Tucker-Zemlin's formulation, two-index potential)
+    Parameters:
+        - n: number of nodes
+        - c[i,j]: cost for traversing arc (i,j)
+        - e[i]: earliest date for visiting node i
+        - l[i]: latest date for visiting node i
+    Returns a model, ready to be solved.
+    """
+    model = Model("tsptw2")
 
     x, u = {}, {}
-    nodes = set(e.keys()) & set(l.keys())  # Only consider nodes that are present in e and l
-
-    for i in nodes:
-        u[i] = model.addVar(lb=e[i], ub=l[i], vtype="C", name="u(%s)" % i)
-        for j in nodes:
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
             if i != j:
                 x[i, j] = model.addVar(vtype="B", name="x(%s,%s)" % (i, j))
+                u[i, j] = model.addVar(vtype="C", name="u(%s,%s)" % (i, j))
 
-    for i in nodes:
-        model.addCons(quicksum(x[i, j] for j in nodes if j != i) == 1, "Out(%s)" % i)
-        model.addCons(quicksum(x[j, i] for j in nodes if j != i) == 1, "In(%s)" % i)
+    for i in range(1, n + 1):
+        model.addCons(quicksum(x[i, j] for j in range(1, n + 1) if j != i) == 1, "Out(%s)" % i)
+        model.addCons(quicksum(x[j, i] for j in range(1, n + 1) if j != i) == 1, "In(%s)" % i)
 
-        for j in nodes:
+    for j in range(2, n + 1):
+        model.addCons(quicksum(u[i, j] + c[i, j] * x[i, j] for i in range(1, n + 1) if i != j) -
+                      quicksum(u[j, k] for k in range(1, n + 1) if k != j) <= 0, "Relate(%s)" % j)
+
+    for i in range(1, n + 1):
+        for j in range(1, n + 1):
             if i != j:
-                M1 = max(l[i] + c[i, j] - e[j], 0)
-                M2 = max(l[i] + min(-c[j, i], e[j] - e[i]) - e[j], 0)
-                model.addCons(u[i] + c[i, j] - M1 * (1 - x[i, j]) + M2 * x[j, i] <= u[j], "LiftedMTZ(%s,%s)" % (i, j))
-
-    for i in nodes:
-        model.addCons(e[i] + quicksum(max(e[j] + c[j, i] - e[i], 0) * x[j, i] for j in nodes if i != j) \
-                      <= u[i], "LiftedLB(%s)" % i)
-
-        model.addCons(u[i] <= l[i] - \
-                      quicksum(max(l[i] - l[j] + c[i, j], 0) * x[i, j] for j in nodes if i != j), \
-                      "LiftedUB(%s)" % i)
+                model.addCons(e[i] * x[i, j] <= u[i, j], "LB(%s,%s)" % (i, j))
+                model.addCons(u[i, j] <= l[i] * x[i, j], "UB(%s,%s)" % (i, j))
 
     model.setObjective(quicksum(c[i, j] * x[i, j] for (i, j) in x), "minimize")
 
@@ -42,25 +45,33 @@ def mtz2tw(n, c, e, l):
 
 
 if __name__ == "__main__":
+    # n = 10
+    # width = 10
     EPS = 1.e-6
     # n = 10
     # width = 10
     # c,x,y,e,l = make_data(n,width)
-    c=expected_dict
+    c = expected_dict
 
     print(c)
     print(e)
     print(l)
 
-    model = mtz2tw(n, c, e, l)
+    print("TWO INDEX MODEL")
+    model = tsptw2(n, c, e, l)
     model.optimize()
-    x, u = model.data
-
-    sol = [i for (v, i) in sorted([(model.getVal(u[i]), i) for i in u])]
-    print("mtz2:")
-    print(sol)
     print("Optimal value:", model.getObjVal())
-    if model.getStatus() == "optimal":
-        sol = [i for (v, i) in sorted([(model.getVal(u[i]), i) for i in u])]
-    else:
-        print("The problem is infeasible or not in the SOLVING stage.")
+    x, u = model.data
+    for (i, j) in x:
+        if model.getVal(x[i, j]) > EPS:
+            print(x[i, j].name, i, j, model.getVal(x[i, j]))
+
+    start_time = [0] * (n + 1)
+    for (i, j) in u:
+        if model.getVal(u[i, j]) > EPS:
+            print(u[i, j].name, i, j, model.getVal(u[i, j]))
+            start_time[j] += model.getVal(u[i, j])
+
+    start = [i for v, i in sorted([(start_time[i], i) for i in range(1, n + 1)])]
+    print(start)
+
--- read_excel.py
+++ read_excel.py
@@ -1,50 +1,27 @@
 import pandas as pd
 import numpy as np
 
-entries_shipment = pd.read_excel("shipments_entries.xlsx", nrows=100)
+#entries_shipment = pd.read_excel("shipments_entries.xlsx")
 
-distance_matrix = pd.read_excel("distanceMatrix.xlsx", index_col=0)
+#distance_matrix = pd.read_excel("distanceMatrix.xlsx",index_col=0)
+"""
 
-# Save the address IDs from both sheets to two separate lists
-address_ids_entries = entries_shipment["AddressId"].tolist()
-address_ids_distance = distance_matrix.index.tolist()
-print(address_ids_entries)
-print(address_ids_distance)
-
-# Find the common address IDs
-common_address_ids = list(set(address_ids_entries) & set(address_ids_distance))
-
-print(common_address_ids)
-
-
+# Extract the unique nodes from shipment details
+unique_address_ids = set(entries_shipment["AddressId"])
 
 # print(unique_address_ids)
-
-
+# Get the common address IDs between distance matrix and unique IDs
+common_address_ids = list(set(distance_matrix.index) & unique_address_ids)
 
 # Filter the entries to only include rows with AddressId in the common_address_ids
-#filtered_entries = entries_shipment[entries_shipment['AddressId'].isin(common_address_ids)]
-
-
+filtered_entries = entries_shipment[entries_shipment['AddressId'].isin(common_address_ids)]
 
 # Create dictionaries for start time and end time
-#start_time_dict = dict(zip(filtered_entries['AddressId'], filtered_entries['Von1']))
-#end_time_dict = dict(zip(filtered_entries['AddressId'], filtered_entries['Bis1']))
+start_time_dict = dict(zip(filtered_entries['AddressId'], filtered_entries['Von1']))
+end_time_dict = dict(zip(filtered_entries['AddressId'], filtered_entries['Bis1']))
 
-#print(start_time_dict)
-#print(end_time_dict)
-
-e={}
-l={}
-
-# Convert timestamps to total seconds since the start of the day
-#for key in start_time_dict:
-#    e[key] = (start_time_dict[key] - pd.Timestamp(start_time_dict[key].date())).total_seconds()
-#    l[key] = (end_time_dict[key] - pd.Timestamp(end_time_dict[key].date())).total_seconds()
-
-#print(e)
-#print(l)
-"""
+# print(start_time_dict)
+# print(end_time_dict)
 
 print(len(common_address_ids))
 # Create a new DataFrame with only the common address IDs as the columns and indices
@@ -67,7 +44,7 @@ for address_id in common_address_ids:
 new_distance_matrix.dropna(how='all', inplace=True)
 new_distance_matrix.dropna(axis=1, how='all', inplace=True)
 
-"""
+
 # Write the new distance matrix to a new Excel file
 # new_distance_matrix.to_excel("new_distance_matrix.xlsx")
 
@@ -89,26 +66,12 @@ reverse_index_mapping = {value: key for key, value in index_mapping.items()}
 
 n = len(index_mapping)
 
-
-
-
-# Create new dictionaries for start time and end time with sequential integers as keys
-#e_seq = {i: e.get(reverse_index_mapping[i], None) for i in range(1, n + 1)}
-#l_seq = {i: l.get(reverse_index_mapping[i], None) for i in range(1, n + 1)}
-
-# For e_seq
-#e_seq_values_not_none = [value for value in e_seq.values() if value is not None]
-
-# For l_seq
-#l_seq_values_not_none = [value for value in l_seq.values() if value is not None]
-
-#print(e_seq_values_not_none)
-#print(l_seq_values_not_none)
-
-new_distance_matrix_with_sequential_integers = pd.read_excel("new_distance_matrix_with_sequential_integers.xlsx", index_col=0)
+# nrows=100, usecols= range(101)
 # print("Index Mapping:", index_mapping)
 # print("Column Mapping:", column_mapping)
 
+
+new_distance_matrix_with_sequential_integers = pd.read_excel("new_distance_matrix_with_sequential_integers.xlsx", nrows=5, usecols= range(6),index_col=0)
 
 # Convert the DataFrame to a nested dictionary
 nested_dict = new_distance_matrix_with_sequential_integers.to_dict()
@@ -122,7 +85,152 @@ for i in nested_dict:
 # Now expected_dict has the desired format
 #print(expected_dict)
 
-# n = len(new_distance_matrix_with_sequential_integers)  # Assuming index_mapping contains the mapping of original IDs to sequential integers
+
+
+# mapping the address ids to sequential integers in the DataFrame
+
+# Convert 'Von1' and 'Bis1' to strings in the format 'YYYY-MM-DD HH:MM:SS'
+entries_shipment['Von1'] = entries_shipment['Von1'].dt.strftime('%Y-%m-%d %H:%M:%S')
+entries_shipment['Bis1'] = entries_shipment['Bis1'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+# Assuming entries_shipment is the DataFrame and "AddressId" is the column with the address IDs
+grouped = entries_shipment.groupby(["AddressId"])
+
+# Create a mapping from grouped address IDs to sequential integers
+address_id_mapping = {group: i for i, group in enumerate(grouped.groups.keys(), start=1)}
+
+
+# Create a new column in the DataFrame with the mapped integers
+entries_shipment["MappedId"] = entries_shipment["AddressId"].map(address_id_mapping)
+
+# Sort the DataFrame by 'AddressId'
+entries_shipment = entries_shipment.sort_values(by='AddressId')
+
+# Select only the columns you need
+selected_columns = entries_shipment[["MappedId","AddressId", "StopZeit", "Von1", "Bis1", "Von2", "Bis2", "Strasse", "Hausnr", "PLZ", "Ort", "Latitude", "Longitude"]]
+
+# Write the selected columns to a new Excel file
+selected_columns.to_excel("shipments_entries_with_mapped_ids.xlsx", index=False)
+# Step 1: Read the new Excel file with the mapped IDs
+
+entries_shipment = pd.read_excel("shipments_entries_with_mapped_ids.xlsx")
+# Assuming common_address_ids is a list of address IDs that you want to keep
+# Step 2: Filter the entries to only include rows with AddressId in the common_address_ids
+filtered_entries = entries_shipment[entries_shipment['AddressId'].isin(common_address_ids)]
+# Step 3: Convert timestamps to total seconds since the start of the day
+filtered_entries['Von1'] = pd.to_datetime(filtered_entries['Von1'])
+filtered_entries['Bis1'] = pd.to_datetime(filtered_entries['Bis1'])
+
+filtered_entries['Von1'] = filtered_entries['Von1'].dt.hour * 3600 + filtered_entries['Von1'].dt.minute * 60 + filtered_entries['Von1'].dt.second
+
+filtered_entries['Bis1'] = filtered_entries['Bis1'].dt.hour * 3600 + filtered_entries['Bis1'].dt.minute * 60 + filtered_entries['Bis1'].dt.second
+# Remove duplicate rows based on 'MappedId'
+
+filtered_entries = filtered_entries.drop_duplicates(subset='MappedId', keep='first')
+
+filtered_entries.to_excel("filtered_shipments_entries_with_mapped_ids.xlsx", index=False)
+
+"""
+# Read the filtered shipments entries file
+filtered_entries = pd.read_excel("filtered_shipments_entries_with_mapped_ids.xlsx", nrows=30)
+
+# Create lists for start time and end time
+start_time_list = filtered_entries['Von1'].tolist()
+end_time_list = filtered_entries['Bis1'].tolist()
+
+# Create dictionaries for start time and end time
+e = {i+1: start_time for i, start_time in enumerate(start_time_list)}
+l = {i+1: end_time for i, end_time in enumerate(end_time_list)}
+
+#print(e)
+#print(l)
+
+n = filtered_entries['MappedId'].nunique()
+#print(n)
+
+"""
+new_distance_matrix = pd.read_excel("new_distance_matrix.xlsx")
+
+
+# Create a DataFrame with address IDs as the index and columns
+df = pd.DataFrame(new_distance_matrix, index=new_distance_matrix.index, columns=new_distance_matrix.columns)
+# Drop the 'Unnamed: 0' column if it exists
+if 'Unnamed: 0' in df.columns:
+    df = df.drop(columns=['Unnamed: 0'])
+# Convert the index and columns to integers
+df.index = df.index.astype(int)
+df.columns = df.columns.astype(int)
+# Set the index to be the same as the columns
+df.index = df.columns
+
+# Sort the DataFrame rows and columns
+df.sort_index(axis=0, inplace=True)
+
+# Save the reordered distance matrix to an Excel file
+df.to_excel("reordered_distance_matrix.xlsx")
+
+df.sort_index(axis=1, inplace=True)
+df.to_excel("reordered_distance_matrix_new.xlsx")
+# Create a mapping from DataFrame's indices to sequential integers
+index_mapping = {value: i + 1 for i, value in enumerate(df.index)}
+column_mapping = {value: i + 1 for i, value in enumerate(df.columns)}
+# Replace the indices of the DataFrame with the sequential integers
+df.index = [index_mapping[i] for i in df.index]
+df.columns = [column_mapping[i] for i in df.columns]
+
+# Save the updated DataFrame to an Excel file
+df.to_excel("final_distance_matrix_new.xlsx")
+
+ """
+
+final_distance_matrix_new=pd.read_excel("final_distance_matrix_new.xlsx",nrows=30, usecols=range(31),index_col=0)
+# Convert the DataFrame to a nested dictionary
+nested_dict = final_distance_matrix_new.to_dict()
+
+# Convert to the expected format
+expected_dict = {}
+for i in nested_dict:
+    for j in nested_dict[i]:
+        expected_dict[(i, j)] = nested_dict[i][j]
+
+# Now expected_dict has the desired format
+#print(expected_dict)
 
 
 
+
+"""
+#method for code
+def read_excel_data():
+    # Your existing code to read the Excel file goes here
+    filtered_entries = pd.read_excel("filtered_shipments_entries_with_mapped_ids.xlsx")
+    final_distance_matrix_new = pd.read_excel("final_distance_matrix_new.xlsx", nrows=len(filtered_entries),
+                                              usecols=range(len(filtered_entries) + 1), index_col=0)
+    # Continue with your existing code
+    return filtered_entries, final_distance_matrix_new
+
+
+def process_data(filtered_entries, final_distance_matrix_new):
+    # Your existing code to process the data goes here
+    # Convert the DataFrame to a nested dictionary\
+
+    # Create lists for start time and end time
+    start_time_list = filtered_entries['Von1'].tolist()
+    end_time_list = filtered_entries['Bis1'].tolist()
+
+    # Create dictionaries for start time and end time
+    e = {i + 1: start_time for i, start_time in enumerate(start_time_list)}
+    l = {i + 1: end_time for i, end_time in enumerate(end_time_list)}
+
+    nested_dict = final_distance_matrix_new.to_dict()
+    # Convert to the expected format
+    expected_dict = {}
+    for i in nested_dict:
+        for j in nested_dict[i]:
+            expected_dict[(i, j)] = nested_dict[i][j]
+
+    n = filtered_entries['MappedId'].nunique()
+
+    # Now expected_dict has the desired format
+    return expected_dict, e, l, n
+"""
